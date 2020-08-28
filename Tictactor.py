d5c1e1d12f6864faadbb8c995d142e366b134987
Thatphum <s6201012620112@email.kmutnb.ac.th>
2020-08-28 11:49:22 +0700

    -Error

--- Tictactor.py
+++ Tictactor.py
@@ -1,100 +1,73 @@
-class Board:
+class Board() :
     def __init__(self):
-        self.listBoard = [[" "," "," "],[" "," "," "],[" "," "," "]] ## create empty board
-        self.player = ""
+        self.listBoard = [[" "," "," "],[" "," "," "],[" "," "," " ]]
+        self.player="o"   #start player
 
-        ## link object together
-        self.printer = Printer(self)
-        self.textInputor = TextInput(self)
+        self.text = TextInput(self)  #instance from class  to Input and drawBoard
+        self.printed = Printer(self)
 
-    def startGame(self):
-        self.round = 1
-        print("Start Game : Tic-Tac-Toe!")
+    def startGame(self) :
+        self.round=1        #starter round
+        self.printed.drawBoard()      #draw empty board
+        while(self.winCheck(self.player)) :       #check if someone win
 
-        try:
-            while (self.round < 10):
-                if self.player == "" or self.player == "O":
-                    ## Player 1 Turn
-                    self.player = "X"
-                    pos = int(input("\nPlayer 1 turn \nEnter your position 'X': "))
+            if self.round >= 9:         #if round over 9 then stop
+                print("Draw")
+                break
+            if self.round%2==1 :        #swap player
+                self.player = "o"
+            else :
+                self.player = "x"
+            print("Player",self.player,"Turn ")  #display turn
+            try :
+                position = int(input("insert position : "))
+                if self.text.placeMarker(self.player, position) : #input to the board
+                    self.round +=1
+            except :
+                print("Invalid Input : Try again")
+            self.printed.drawBoard()          #draw new board
 
-                else :
-                    ## Player 2 Turn
-                    self.player = "O"
-                    pos = int(input("\nPlayer 2 turn \nEnter your position 'O': "))
-
-                self.textInputor.placeMarker(self.player, pos)  ## place marker in position that user entered
-
-                self.printer.drawBoard()  ## display board
-
-                if not(self.winCheck(self.player)):
-                    ## no one win
-                    self.round += 1
-
-                else:
-                    ## when have the winner
-                    print("\n!!! End Game !!!")
-                    break
-
-            else:
-                print("\nNo player win :( \n!!! Game Over !!!")
-
-        except ValueError:
-            ## case when entered wrong value, wrong type
-            print("\nYou entered wrong value....Game will start again")
+        if int(input("""Enter "1" to play  again else to stop """))==1 :
             self.clearBoard()
-            self.player = ""
-            self.startGame()
+            self.start()
 
+    def retrieveValue(self,position) :  #getter method
+        return self.listBoard[int((position-1)/3)][(position-1)%3]
 
-    def setBoard(self, player, position):
-        # place marker X,O at position that user entered
+    def setBoard(self,player,position) :     #setter setBoard
+        self.listBoard[int((position-1)/3)][(position-1)%3] = player
 
-        if position <= 3 and self.listBoard[0][position-1] == " ":
-            self.listBoard[0][position-1] = self.player
-
-        elif 3 < position <= 6 and self.listBoard[1][position-4] == " ":
-            self.listBoard[1][position-4] = self.player
-
-        elif 6 < position <= 9 and self.listBoard[2][position-7] == " ":
-            self.listBoard[2][position-7] = self.player
-
-        else:
+    def winCheck(self,player) :  #if someone wins return false to stop loop
+        if self.listBoard[0][0] == self.listBoard[0][1] == self.listBoard[0][2] and self.listBoard[0][2] != " ":
+            print("Player",player,"WINS!")
             return False
+        elif self.listBoard[1][0] == self.listBoard[1][1] == self.listBoard[1][2] and self.listBoard[1][2] != " ":
+            print("Player",player,"WINS!")
+            return False
+        elif self.listBoard[2][0] == self.listBoard[2][1] == self.listBoard[2][2] and self.listBoard[2][2] != " ":
+            print("Player",player,"WINS!")
+            return False
+        elif self.listBoard[0][0] == self.listBoard[1][0] == self.listBoard[2][0] and self.listBoard[2][0] != " ":
+            print("Player",player,"WINS!")
+            return False
+        elif self.listBoard[0][1] == self.listBoard[1][1] == self.listBoard[2][1] and self.listBoard[2][1] != " ":
+            print("Player",player,"WINS!")
+            return False
+        elif self.listBoard[0][2] == self.listBoard[1][2] == self.listBoard[2][2] and self.listBoard[2][2] != " ":
+            print("Player",player,"WINS!")
+            return False
+        elif self.listBoard[0][0] == self.listBoard[1][1] == self.listBoard[2][2] and self.listBoard[2][2] != " ":
+            print("Player",player,"WINS!")
+            return False
+        elif self.listBoard[0][2] == self.listBoard[1][1] == self.listBoard[2][0] and self.listBoard[2][0] != " ":
+            print("Player",player,"WINS!")
+            return False
+        else:
+            return True
 
-    def retrieveValue(self, position): ## get value form listBoard
-        if position <= 3 :
-            return self.listBoard[0][position-1]
+    def clearBoard(self) :
+        self.listBoard = [[" "," "," "],[" "," "," "],[" "," "," " ]]
 
-        elif 3 < position <= 6 :
-            return self.listBoard[1][position-4]
-
-        elif 6 < position <= 9 :
-            return self.listBoard[2][position-7]
-
-    def winCheck(self, player):
-        # check winner
-        # return True when has winner
-
-        for i in range(len(self.listBoard)):
-            if (self.listBoard[0][0] == self.listBoard[1][1] == self.listBoard[2][2] != " ") or \
-            (self.listBoard[0][2] == self.listBoard[1][1] == self.listBoard[2][0] != " ") or \
-            (self.listBoard[i][0] == self.listBoard[i][1] == self.listBoard[i][2] != " ") or \
-            (self.listBoard[0][i] == self.listBoard[1][i] == self.listBoard[2][i] != " "):
-
-                if player == "X":
-                    playerTurn = 1
-                else:
-                    playerTurn = 2
-
-                print("\n------ Player {} ({}) is winner!------".format(playerTurn, player))
-                return True
-
-        ## No player has win
-        return False
-
-    def clearBoard(self):
-        self.board = [[" "," "," "],[" "," "," "],[" "," "," "]]
 class TextInput() :
     def __init__(self, object) :
         self.board=object
@@ -122,4 +95,4 @@ class Printer() :
         print(self.board.retrieveValue(7) + " | " + self.board.retrieveValue(8) + " | " + self.board.retrieveValue(9)+"\n")
 
 game1=Board()#instance obj from Board
-game1.startGame()#start game
+game1.start()#start game
